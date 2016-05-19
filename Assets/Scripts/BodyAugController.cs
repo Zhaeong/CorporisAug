19cdf972daec24314c713bd870b2fe12c259c2c5
Zhaeong <owen_zzz@hotmail.com>
2016-05-19 01:17:12 -0400

    added limp switching

---
+++ Assets/Scripts/BodyAugController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BodyAugController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "LeftArm")
+        {
+            
+            Debug.Log("Sucesss");
+            foreach (Transform child in transform)
+            {
+                if (child.tag == "PlayerLeftArm")
+                {
+                    other.transform.parent = child.transform;
+                    other.transform.localPosition = new Vector3(-0.1f, -0.15f, 0);
+                    child.GetComponent<SpriteRenderer>().enabled = false;
+                }
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Animator Anim;
+    private bool isfacingleft, wasfacingleft;
+    string currentDirection, previousDirection;
+	// Use this for initialization
+	void Start () {
+
+        isfacingleft = true;
+        wasfacingleft = true;
+        Anim = GetComponent<Animator>();
+        
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float input_x = Input.GetAxisRaw("Horizontal");
+        float input_y = Input.GetAxisRaw("Vertical");
+        bool isWalking = (Mathf.Abs(input_x) + Mathf.Abs(input_y)) > 0;
+        Anim.SetBool("walkingLeft", input_x < 0);
+
+        if (input_x == -1)
+        {
+            isfacingleft = true;
+        }
+        else if (input_x == 1)
+        {
+            isfacingleft = false;
+        }
+
+        if (isfacingleft != wasfacingleft)
+        {
+            flip();
+            wasfacingleft = isfacingleft;
+        }
+
+
+
+        Anim.SetBool("isWalking", isWalking);
+        if (isWalking)
+        {
+            Anim.SetFloat("x", input_x);
+            Anim.SetFloat("y", input_y);
+            transform.position += new Vector3(input_x, input_y, 0).normalized * Time.deltaTime;
+        }
+    }
+
+    void flip()
+    {
+        foreach (Transform child in transform)
+        {
+            Vector3 theScale = child.transform.localScale;
+            theScale.x *= -1;
+            child.transform.localScale = theScale;
+        }
+    }
+}
