a56fa6392e7112b597714cc319da5982e7c24141
Zhaeong <owen_zzz@hotmail.com>
2016-05-31 02:23:33 -0400

    added camera script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public Camera gameCamera;
+
+
+    // Use this for initialization
+    void Start () {
+        Vector3 startingPos = new Vector3(transform.position.x, transform.position.y, -2);
+
+        gameCamera.transform.position = startingPos;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 startingPos = new Vector3(transform.position.x, transform.position.y, -2);
+
+        gameCamera.transform.position = startingPos;
+
+    }
+}
