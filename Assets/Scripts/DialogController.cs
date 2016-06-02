1e94b15ca18b8b87791a0bf8b85e14501fa9ae69
Zhaeong <owen_zzz@hotmail.com>
2016-06-02 01:34:16 -0400

    added text stuffs

---
+++ Assets/Scripts/DialogController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogController : MonoBehaviour {
+
+    public TextAsset theText;
+    public TextController textController;
+
+	// Use this for initialization
+	void Start () {
+        textController = FindObjectOfType<TextController>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("withing");
+        if (other.tag == "Player")
+        {
+            textController.LoadText(theText);
+
+           
+        }
+    }
+}
---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+    public GameObject textBox;
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+    public PlayerController player;
+
+    public bool isActive;
+
+	// Use this for initialization
+	void Start () {
+
+        if (isActive)
+        {
+            EnableTextBox();
+        }
+        else
+        {
+            DisableTextBox();
+        }
+
+
+        player = FindObjectOfType<PlayerController>();
+
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if(endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!isActive)
+        {
+            DisableTextBox();
+            return;
+        }
+        else
+        {
+            EnableTextBox();
+
+            theText.text = textLines[currentLine];
+            
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                currentLine += 1;
+            }
+
+            if (currentLine > endAtLine)
+            {
+                DisableTextBox();
+            }
+        }
+	
+	}
+
+    public void EnableTextBox()
+    {
+        textBox.SetActive(true);
+    }
+
+    public void DisableTextBox()
+    {
+        textBox.SetActive(false);
+    }
+
+    public void LoadText(TextAsset theText)
+    {
+        if (theText != null)
+        {
+            textLines = new string[1];
+            textLines = theText.text.Split('\n');
+            currentLine = 0;
+            isActive = true;
+        }
+    }
+
+}
