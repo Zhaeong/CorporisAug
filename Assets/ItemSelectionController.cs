96364ee18052439cefa5cc25d90b778f878429d5
Zhaeong <owen_zzz@hotmail.com>
2016-07-18 18:37:37 -0400

    added item selection panel

---
+++ Assets/ItemSelectionController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSelectionController : MonoBehaviour {
+
+    public GameObject[] itemlist; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        itemlist = GameObject.FindGameObjectsWithTag("Limb");
+	
+	}
+}
