869aab693b69174f21e828e93b740c02c7d83417
Zhaeong <owen_zzz@hotmail.com>
2016-08-26 04:00:17 -0400

    wiped errthing

---
+++ Assets/FirstPersonController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+
+    CharacterController CC;
+	// Use this for initialization
+	void Start () {
+        CC = gameObject.GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 vMovement = new Vector3(x, 0, z);
+        CC.Move(vMovement);
+    }
+}
