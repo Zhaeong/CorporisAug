9312de8807dd5bd06a611470c9b80b5df9750914
Zhaeong <owen_zzz@hotmail.com>
2016-08-27 03:37:21 -0400

    added basic inventory

---
+++ Assets/InventoryController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryController : MonoBehaviour {
+
+    SpriteRenderer SR;
+    public bool InventoryOn;
+	// Use this for initialization
+	void Start () {
+        SR = gameObject.GetComponent<SpriteRenderer>();
+        SR.enabled = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            if (SR.enabled)
+                SR.enabled = false;
+            else
+                SR.enabled = true;
+        }
+	    
+	}
+
+
+}
