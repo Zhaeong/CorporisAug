0805a95e9ac915ed43201d6fd2ecf11a1c1a3756
Zhaeong <owen_zzz@hotmail.com>
2016-08-28 15:17:46 -0400

    better billboard

--- Assets/BillboardBehaviour.cs
+++ Assets/BillboardBehaviour.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class BillboardBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    GameObject MainCam;
+    // Use this for initialization
+    void Start () {
+        MainCam = GameObject.FindGameObjectWithTag("MainCamera");
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.LookAt(Camera.main.transform.position, -Vector3.up);
+        //Should use look at because multiple objects would be weird, will be at angles to camera
+        //transform.LookAt(Camera.main.transform.position, -Vector3.up);
+        transform.forward = MainCam.transform.forward;
 
     }
 }
