b6b31c9035eb0fa504b9bb48e2eb3447fdb2662e
Zhaeong <owen_zzz@hotmail.com>
2016-08-28 15:20:50 -0400

    lateupdate

--- Assets/BillboardBehaviour.cs
+++ Assets/BillboardBehaviour.cs
@@ -14,7 +14,12 @@ public class BillboardBehaviour : MonoBehaviour {
 	void Update () {
         //Should use look at because multiple objects would be weird, will be at angles to camera
         //transform.LookAt(Camera.main.transform.position, -Vector3.up);
-        transform.forward = MainCam.transform.forward;
+        
 
+    }
+
+    void LateUpdate()
+    {
+        transform.forward = MainCam.transform.forward;
     }
 }
