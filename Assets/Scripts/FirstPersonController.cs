4feef56df3b6fc64ef3e6abfa163244b9ad30f93
Zhaeong <owen_zzz@hotmail.com>
2016-08-29 20:21:35 -0400

    some formatting

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FirstPersonController : MonoBehaviour {
+public class FirstPersonController : MonoBehaviour
+{
     public Camera mainCamera;
     public float MovementSpeed = 10.0f;
     public float Gravity = 20.0f;
@@ -16,18 +17,18 @@ public class FirstPersonController : MonoBehaviour {
     private bool isJumping;
     CharacterController CC;
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         CC = gameObject.GetComponent<CharacterController>();
         yaw = 0.0f;
         pitch = 0.0f;
         isJumping = false;
     }
-	
-	// Update is called once per frame
-	void Update () {
 
+    // Update is called once per frame
+    void Update()
+    {
         curHeight = transform.position.y;
 
         float x = Input.GetAxis("Horizontal");
@@ -41,33 +42,26 @@ public class FirstPersonController : MonoBehaviour {
         Vector3 MoveDirectionForward;
         Vector3 Movement = Vector3.zero;
 
-
         MoveDirectionForward = mainCamera.transform.forward;
         MoveDirectionForward = new Vector3(MoveDirectionForward.x, 0, MoveDirectionForward.z);
 
         Vector3 MoveRight = Vector3.Cross(MoveDirectionForward, Vector3.up);
         Vector3 MoveLeft = -MoveRight;
 
+        if (z != 0.0f)
+        {
+            CC.Move(z * MoveDirectionForward * MovementSpeed * Time.deltaTime);
+        }
+        if (x != 0.0f)
+        {
+            CC.Move(x * MoveLeft * MovementSpeed * Time.deltaTime);
+        }
 
-            if (z != 0.0f)
-            {
-                CC.Move(z * MoveDirectionForward * MovementSpeed * Time.deltaTime);
-            }
-            if (x != 0.0f)
-            {
-                CC.Move(x * MoveLeft * MovementSpeed * Time.deltaTime);
-            }
-
-            if (Input.GetButton("Jump") && CC.isGrounded)
-            {
-
-                    tarHeight = curHeight + JumpHeight;
-                    isJumping = true;
-                
-            }
-
-
-
+        if (Input.GetButton("Jump") && CC.isGrounded)
+        {
+            tarHeight = curHeight + JumpHeight;
+            isJumping = true;
+        }
 
         if (isJumping)
         {
@@ -80,13 +74,9 @@ public class FirstPersonController : MonoBehaviour {
                 CC.Move(Vector3.up * Time.deltaTime * JumpSpeed);
             }
         }
-
-        if (!isJumping)
+        else
         {
             CC.Move(Vector3.down * Time.deltaTime * Gravity);
-        }
-
-
-
+        }        
     }
 }
