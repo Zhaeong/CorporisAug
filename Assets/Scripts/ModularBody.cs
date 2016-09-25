7bc33dfb06a5eb9710b51657ae014bb68f9f6cc3
Daniel Ledlow <ledlowdaniel@gmail.com>
2016-09-25 14:20:04 -0400

    Added ModularBody component and BodyTemplate asset type.

---
+++ Assets/Scripts/ModularBody.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BillboardBehaviour))]
+public class ModularBody : MonoBehaviour {
+
+    public BodyTemplate Template;
+    public Sprite[] Sprites;
+
+	// Use this for initialization
+	void Start () {
+	    for (int i = 0; i < Sprites.Length && i < Template.BodyParts.Length; ++i)
+        {
+            GameObject obj = new GameObject();
+            obj.transform.SetParent(transform);
+            var renderer = obj.AddComponent<SpriteRenderer>();
+            renderer.sprite = Sprites[i];
+            obj.transform.localPosition = Template.BodyParts[i];
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/ScriptableObjects/BodyTemplate.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "BodyTemplate", menuName = "BodyTemplate", order = 1)]
+public class BodyTemplate : ScriptableObject
+{
+    public Vector2[] BodyParts;
+}
